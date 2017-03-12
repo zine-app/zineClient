59fd31cd43c1b0a750b25992453b47455827e30a
garytryan <gary.thomas.ryan@gmail.com>
2017-03-12 12:00:13 -0700

    styled home page layout

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import UserProfile from 'app/containers/UserProfile'
+import 'app/styles/home'
 
 export default () =>
   <div>
-    <div></div>
-    <div></div>
+    <div className='zine-sidebar--container'></div>
+    <div className='zine-main--container'></div>
   </div>
