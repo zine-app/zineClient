0e1498374be34196560c14c98cb2c105c204313c
garytryan <gary.thomas.ryan@gmail.com>
2017-03-13 10:55:09 -0700

    close #9

--- src/app/reducers/user.ts
+++ src/app/reducers/user.ts
@@ -5,46 +5,66 @@ import { pick } from 'lodash'
 const initialState = createUser()
 
 export const userReducer = handleActions({
-  "USER:SET": (state, action) =>
-    state.merge(
-      pick(action.payload,
-        [
-          'name',
-          'email',
-          'profileImageURL',
-          'facebookUserId',
-          'facebookUserAccessToken'
-        ]
-    )),
+  "USER:SET": {
+    next: (state, action) =>
+      state.merge(
+        pick(action.payload,
+          [
+            'name',
+            'email',
+            'profileImageURL',
+            'facebookUserId',
+            'facebookUserAccessToken'
+          ]
+      )),
 
-  "USER:SAVE:RESPONSE": (state, action) =>
-    state.merge(
-      pick(action.payload,
-        [
-          'name',
-          'email',
-          'profileImageURL',
-          'facebookUserId',
-          'facebookUserAccessToken'
-        ]
-    )),
+    throw: (state, action) => state
+  },
 
-  "AUTH:ZINE:SIGNUP:RESPONSE": (state, action) =>
-    state.merge(
-      pick(action.payload,
-        [
-          'name',
-          'email',
-          'profileImageURL',
-          'facebookUserId',
-          'facebookUserAccessToken'
-        ]
-    )),
 
-  "AUTH:LOGOUT:RESPONSE": (state, action) =>
-    state.merge({
-      name: '',
-      email: '',
-      profileImageURL: ''
-    })
+  "USER:SAVE:RESPONSE": {
+    next: (state, action) =>
+      state.merge(
+        pick(action.payload,
+          [
+            'name',
+            'email',
+            'profileImageURL',
+            'facebookUserId',
+            'facebookUserAccessToken'
+          ]
+      )),
+
+    throw: (state, action) => state
+  },
+
+
+  "AUTH:ZINE:SIGNUP:RESPONSE": {
+    next: (state, action) =>
+      state.merge(
+        pick(action.payload,
+          [
+            'name',
+            'email',
+            'profileImageURL',
+            'facebookUserId',
+            'facebookUserAccessToken'
+          ]
+      )),
+
+    throw: (state, action) => state
+  },
+
+
+  "AUTH:LOGOUT:RESPONSE": {
+    next: (state, action) =>
+      state.merge({
+        name: '',
+        email: '',
+        profileImageURL: ''
+      }),
+
+    throw: (state, action) => state
+  }
+
 }, initialState)
