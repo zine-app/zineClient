c3f4ce70fd8e5e356131fa4bbc6010ac8f0c5edd
garytryan <gary.thomas.ryan@gmail.com>
2017-04-04 16:14:02 -0700

    close #49

--- src/app/containers/sidebar/groups/OwnZines.tsx
+++ src/app/containers/sidebar/groups/OwnZines.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 import OwnZines from 'app/components/sidebar/groups/OwnZines'
+import getMyUser from 'app/selectors/me/getMyUser'
 
 const mapStateToProps = state => ({
-  zines: state.get('zines').filter(zine => !zine.deleted)
+  zines: state.get('zines')
+    .filter(zine => !zine.deleted)
+    .filter(zine => zine.ownerId === getMyUser(state).id)
 })
 
 export default connect(mapStateToProps)(OwnZines)
