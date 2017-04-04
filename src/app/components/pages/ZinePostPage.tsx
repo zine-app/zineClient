73ff1b2a95cfee407a871e0d92d36da210017436
garytryan <gary.thomas.ryan@gmail.com>
2017-04-03 18:17:46 -0700

    some styling for post page

--- src/app/components/pages/ZinePostPage.tsx
+++ src/app/components/pages/ZinePostPage.tsx
@@ -1,7 +1,18 @@
 import * as React from 'react'
 import MasterPage from 'app/components/ZineMasterPage'
+import PostReader from 'app/components/PostReader'
+import 'app/styles/zinePostPage'
 
 export default ({ zine, user, post }) =>
   <MasterPage zine={zine} user={user} >
-    { post && <h1>{ post.title }</h1> }
+    <div className="zine--post-page--container">
+      { post && <h1 className="zine--post-page--title">{ post.title }</h1> }
+      {
+        (post && post.body) ?
+          <div className="zine--post-page--body">
+            <PostReader rawEditorState={post.body} />
+          </div> :
+          null
+      }
+    </div>
   </MasterPage>
