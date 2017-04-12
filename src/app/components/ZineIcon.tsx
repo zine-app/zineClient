d4d52d062f10ee1c657c2df6724685ceb4add499
garytryan <gary.thomas.ryan@gmail.com>
2017-04-12 15:43:29 -0700

    sidebar icons use smaller images

--- src/app/components/ZineIcon.tsx
+++ src/app/components/ZineIcon.tsx
@@ -1,13 +1,43 @@
 import * as React from 'react'
 import ProfileIcon from 'app/icons/profile'
 import 'app/styles/zineIcon'
+import cloudinary from 'app/utils/cloudinary'
 
+const calculateImageWidth = size => {
+  const sizeToWidthMap = {
+    small: 30,
+    medium: 60,
+    large: 100
+  }
 
-export default ({ url="", size="medium" }:{ url?:string, size?:string }) =>
+  return sizeToWidthMap[size]
+}
+
+const style = ({ backgroundImageUrl, backgroundImageSize }) => {
+  let backgroundImage = 'none'
+
+  if(backgroundImageUrl) {
+    const url = cloudinary({
+      url: backgroundImageUrl,
+      width: calculateImageWidth(backgroundImageSize),
+      height: calculateImageWidth(backgroundImageSize)
+    })
+
+    backgroundImage = `url('${url}')`
+  }
+
+  return {
+    backgroundImage
+  }
+}
+
+export default ({ url="", size="medium", children=null }:{ url?:string, size?:string, children?:any }) =>
   <div
     className={`zine-icon__${size}`}
-    style={{
-      backgroundImage: url ? `url(${url})` : 'none'
-    }}
+    style={style({
+      backgroundImageUrl: url,
+      backgroundImageSize: size
+    })}
   >
+  { children }
   </div>
